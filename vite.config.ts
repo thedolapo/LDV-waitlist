2fef37b114795874667504d243a66eb455a7d4f7
Dolapo Ayoade <dolapoayoade@macbookpro.home>
2026-02-08 14:10:45 +0000

    Set base to / for ldvsupply.com root domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Project URL: https://thedolapo.github.io/LDV-waitlist/
-  // For ldvsupply.com only: set base: '/' and push (then theolapo.github.io/LDV-waitlist will 404 on assets)
-  base: '/LDV-waitlist/',
+  // For ldvsupply.com at root. To use theolapo.github.io/LDV-waitlist/ instead, set base: '/LDV-waitlist/'
+  base: '/',
 })
