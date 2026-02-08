053ca8077cd93df7c8fb6a0e2b4381c6eb59b772
Dolapo Ayoade <dolapoayoade@macbookpro.home>
2026-02-08 03:36:46 +0000

    Restore base /LDV-waitlist/ so GitHub Pages assets load

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Custom domain ldvsupply.com (root). For theolapo.github.io/LDV-waitlist/ use base: '/LDV-waitlist/'
-  base: '/',
+  // Project URL: https://thedolapo.github.io/LDV-waitlist/
+  // For ldvsupply.com only: set base: '/' and push (then theolapo.github.io/LDV-waitlist will 404 on assets)
+  base: '/LDV-waitlist/',
 })
